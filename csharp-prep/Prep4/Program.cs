871fd82e082a368b24a4fe720c3c36e9e5eadd60
tfotr241290 <franco_9024@hotmail.com>
2023-01-11 11:50:49 -0600

    completed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+        List<int> numbers = new List<int>();
+
+        while (number != 0){
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine()); 
+            if (number != 0){
+                numbers.Add(number);
+            }
+            
+        }
+        int total = numbers.Sum();
+        double average = numbers.Average();
+        Console.WriteLine($"The total sum is: {total}");
+        int count = numbers.Count();
+        float av = ((float)total) / count;
+        Console.WriteLine($"The average is: {av}");
+        int maxNumber = numbers.Max();
+        Console.WriteLine($"The largest number is {maxNumber}");
+
+
     }
 }
