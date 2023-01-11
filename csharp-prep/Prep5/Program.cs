add61046c5ed3b474dad00674fc8eef158175bcd
tfotr241290 <franco_9024@hotmail.com>
2023-01-11 13:55:10 -0600

    prep 5 completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+        string theName = PromptUserName();
+        int theNumber = PromptUserNumber();
+        double sqrtNumber = SquareNumber(theNumber);
+        DisplayResult(theName,sqrtNumber);
+
+        static void DisplayWelcome(){
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName(){
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber(){
+            Console.Write("What is your favorite number? ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static double SquareNumber(int num){
+            return Math.Sqrt(num);
+        }
+
+        static void DisplayResult(string name, double sqrt){
+            Console.WriteLine($"{name}, the square of your number is {sqrt}");
+        }
+
     }
 }
